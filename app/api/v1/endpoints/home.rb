b0b59173ee33088ff35e6645e7b90ca3b5fab036
Hassan Ahmed <hassanahmed@Hassan.local>
2020-01-25 13:04:13 +0100

    In the beginning, there was nothing. And God said, 'Let there be matrix'.

--- app/api/v1/endpoints/home.rb
+++ app/api/v1/endpoints/home.rb
@@ -3,7 +3,10 @@ module V1
     class Home < Grape::API
       include V1::Headers
       resources :home do
+<<<<<<< HEAD
         desc 'Application health check.'
+=======
+>>>>>>> In the beginning, there was nothing. And God said, 'Let there be matrix'.
         get do
           home = { hello: 'world!' }
           present home, with: V1::Entities::HomeEntity
